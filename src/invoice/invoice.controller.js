7da94057a4b8474ca3be35c00e8c17d395eadeef
Mario Rodriguez <mrodriguez-2023292@kinal.edu.gt>
2025-03-05 10:27:28 -0600

    Se implemento la generacion de factura en PDF

--- src/invoice/invoice.controller.js
+++ src/invoice/invoice.controller.js
@@ -2,6 +2,14 @@
 
 import Invoice from "./invoice.model.js"
 import shoppingCart from "../shoppingCart/shoppingCart.model.js"
+import PDFDocument from "pdfkit"
+import fs from "fs"
+import path from "path"
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 export const generateInvoice = async (req, res) => {
     try {
@@ -35,17 +43,49 @@ export const generateInvoice = async (req, res) => {
         }
 
         const newInvoice = await Invoice.create(invoice)
-
+        
         await shoppingCart.findByIdAndUpdate(id, { 
             status: "completed",
             products: [],
             totalAmount: 0 
         })
 
-        res.status(200).json({
-            success: true,
-            message: "Compra completada con éxito",
-            invoice: newInvoice
+        const doc = new PDFDocument()
+
+        const invoicePDFPath = path.join(__dirname, `../../invoices/invoice_${newInvoice._id}.pdf`)
+        const stream = fs.createWriteStream(invoicePDFPath)
+        doc.pipe(stream)
+
+        doc.fontSize(20).text("Factura de Compra", { align: "center" })
+        doc.moveDown()
+        doc.fontSize(14).text(`Fecha: ${new Date().toLocaleString()}`)
+        doc.moveDown()
+        doc.fontSize(14).text(`Cliente: ${cart.client}`)
+        doc.moveDown()
+        doc.moveTo(50, doc.y).lineTo(550, doc.y).stroke()
+        doc.moveDown()
+        doc.fontSize(14).text("Productos", { align: "center" })
+        doc.moveDown()
+        doc.moveTo(50, doc.y).lineTo(550, doc.y).stroke()
+        doc.moveDown()
+        cart.products.forEach(item => {
+            doc.text(`${item.name} - Cantidad: ${item.quantity} - Precio: ${item.price} - Total: ${item.totalPrice}`)
+        })
+        doc.moveDown(25)
+        doc.moveTo(50, doc.y).lineTo(550, doc.y).stroke()
+        doc.moveDown()
+        doc.text(`Total a pagar: ${cart.totalAmount}`)
+        doc.moveDown()
+
+        doc.end()
+
+        stream.on("finish", () => {
+            res.status(200).json({
+                success: true,
+                message: "Compra completada con éxito",
+                invoice: newInvoice,
+                pdfPath: invoicePDFPath 
+            })
         })
     } catch (error) {
         res.status(500).json({
